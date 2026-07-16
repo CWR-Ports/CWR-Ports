42007d8e2490f59b9cd4bb0edb501676fd93f4c5
Ixee <buffjokester27@gmail.com>
2026-07-17 03:27:19 +0530

    Android: Improved Touchscreen Layout

--- engine/Poseidon/Input/InputProcessingSdl.cpp
+++ engine/Poseidon/Input/InputProcessingSdl.cpp
@@ -41,31 +41,40 @@ enum {
     VB_ADS_LOOK = 4,
     VB_ACTION = 5,
     VB_KEY = 6,
-    VB_ZOOM_LOOK = 7
+    VB_ZOOM_LOOK = 7,
+    VB_TAPHOLD = 8,
+    VB_TOGGLE = 9
 };
 
 struct VirtualButtonDef {
     const char* name;
     float x, y, r;
     int type;
-    int data; 
+    int data;
+    int data2;
 };
 
 VirtualButtonDef g_mobileButtons[] = {
+    {"Fire", 0.08f, 0.33f, 0.09f, VB_FIRE_LOOK, 0, 0},
     {"Move", 0.15f, 0.7f, 0.12f, VB_WASD, 0},
-    {"ADS", 0.08f, 0.35f, 0.06f, VB_KEY, SDL_SCANCODE_V},
-    {"Zoom", 0.08f, 0.15f, 0.06f, VB_ZOOM_LOOK, 0},
+    {"ADS", 0.90f, 0.52f, 0.07f, VB_KEY, SDL_SCANCODE_V},
+    {"Zoom", 0.73f, 0.50f, 0.07f, VB_ZOOM_LOOK, 0},
     {"Fire", 0.85f, 0.65f, 0.09f, VB_FIRE_LOOK, 0},
-    {"Action\n(Drag)", 0.82f, 0.45f, 0.06f, VB_ACTION, 0},
+    {"Action\n(Drag)", 0.82f, 0.5f, 0.06f, VB_ACTION, 0},
+    {"Freelook", 0.965f,0.55f, 0.045f, VB_LOOK, SDL_SCANCODE_LALT, 0},
     {"Reload", 0.95f, 0.35f, 0.05f, VB_KEY, SDL_SCANCODE_R},
-    {"Q", 0.93f, 0.65f, 0.04f, VB_KEY, SDL_SCANCODE_Q},
-    {"Crouch", 0.95f, 0.85f, 0.05f, VB_KEY, SDL_SCANCODE_X}, // INTEGRATE IT WITH PRONE (Z), TAP TO CROUCH, HOLD TO PRONE
-    {"Time", 0.70f, 0.08f, 0.04f, VB_KEY, SDL_SCANCODE_O},
-    {"Compass", 0.79f, 0.08f, 0.04f, VB_KEY, SDL_SCANCODE_K},
-    {"Map", 0.88f, 0.08f, 0.04f, VB_KEY, SDL_SCANCODE_M},
-    {"ESC", 0.96f, 0.08f, 0.03f, VB_KEY, SDL_SCANCODE_ESCAPE},
-    {"Bino", 0.82f, 0.20f, 0.04f, VB_KEY, SDL_SCANCODE_B},
-    {"Firemode", 0.95f, 0.20f, 0.04f, VB_KEY, SDL_SCANCODE_SPACE},
+    {"Q", 0.96f, 0.69f, 0.04f, VB_KEY, SDL_SCANCODE_Q},
+    {"Crouch", 0.95f, 0.85f, 0.06f, VB_KEY, SDL_SCANCODE_X}, // INTEGRATE IT WITH PRONE (Z), TAP TO CROUCH, HOLD TO PRONE
+    {"Prone", 0.85f, 0.85f, 0.06f, VB_KEY, SDL_SCANCODE_Z},
+    {"Time", 0.70f, 0.08f, 0.05f, VB_KEY, SDL_SCANCODE_O},
+    {"Compass", 0.79f, 0.08f, 0.05f, VB_KEY, SDL_SCANCODE_K},
+    {"Map", 0.88f, 0.08f, 0.05f, VB_KEY, SDL_SCANCODE_M},
+    {"ESC", 0.96f, 0.08f, 0.05f, VB_KEY, SDL_SCANCODE_ESCAPE},
+    {"Bino", 0.79f, 0.20f, 0.06f, VB_KEY, SDL_SCANCODE_B},
+    {"Firemode", 0.96f, 0.20f, 0.06f, VB_KEY, SDL_SCANCODE_SPACE},
+    {"TP/FP", 0.70f, 0.20f, 0.05f, VB_KEY, SDL_SCANCODE_KP_ENTER},
+    {"Shift", 0.96f, 0.62f, 0.06f, VB_KEY, SDL_SCANCODE_LSHIFT},
+    {"Chat/Channels", 0.88f, 0.20f, 0.05f, VB_KEY, SDL_SCANCODE_SLASH},
 };
 extern const int g_numMobileButtons = sizeof(g_mobileButtons) / sizeof(g_mobileButtons[0]);
 
