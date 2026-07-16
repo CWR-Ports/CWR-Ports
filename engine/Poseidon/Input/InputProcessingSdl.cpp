c02b4bacad65c0a3455f46c3c9fc4cc472fc2fe5
Ixee <buffjokester27@gmail.com>
2026-07-17 04:26:01 +0530

    Android: Layout Improvements and Temporary Crouch Workaround

--- engine/Poseidon/Input/InputProcessingSdl.cpp
+++ engine/Poseidon/Input/InputProcessingSdl.cpp
@@ -57,14 +57,14 @@ struct VirtualButtonDef {
 VirtualButtonDef g_mobileButtons[] = {
     {"Fire", 0.08f, 0.33f, 0.11f, VB_FIRE_LOOK, 0, 0}, // LEFT FIRE
     {"Move", 0.15f, 0.7f, 0.12f, VB_WASD, 0},
-    {"ADS", 0.90f, 0.52f, 0.07f, VB_KEY, SDL_SCANCODE_V},
-    {"Zoom", 0.73f, 0.50f, 0.07f, VB_ZOOM_LOOK, 0},
+    {"ADS", 0.94f, 0.45f, 0.07f, VB_KEY, SDL_SCANCODE_V},
+    {"Zoom", 0.78f, 0.49f, 0.07f, VB_ZOOM_LOOK, 0},
     {"Fire", 0.85f, 0.65f, 0.09f, VB_FIRE_LOOK, 0},
-    {"Action\n(Drag)", 0.82f, 0.5f, 0.06f, VB_ACTION, 0},
+    {"Action\n(Drag)", 0.875f, 0.47f, 0.07f, VB_ACTION, 0},
     {"Freelook", 0.95f,0.50f, 0.05f, VB_LOOK, SDL_SCANCODE_LALT, 0},
     {"Reload", 0.95f, 0.35f, 0.06f, VB_KEY, SDL_SCANCODE_R},
     {"Q", 0.95f, 0.70f, 0.05f, VB_KEY, SDL_SCANCODE_Q},
-    {"Crouch", 0.95f, 0.85f, 0.06f, VB_KEY, SDL_SCANCODE_X}, // INTEGRATE IT WITH PRONE (Z), TAP TO CROUCH, HOLD TO PRONE
+    {"Crouch", 0.95f, 0.85f, 0.06f, VB_KEY, SDL_SCANCODE_Q}, // INTEGRATE IT WITH PRONE (Z), TAP TO CROUCH, HOLD TO PRONE
     {"Prone", 0.85f, 0.85f, 0.06f, VB_KEY, SDL_SCANCODE_Z},
     {"Time", 0.70f, 0.08f, 0.05f, VB_KEY, SDL_SCANCODE_O},
     {"Compass", 0.79f, 0.08f, 0.05f, VB_KEY, SDL_SCANCODE_K},
